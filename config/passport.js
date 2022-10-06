60f9d4196a14b9c0ed437cace61e9b17ab54bada
jshprintz <jshprintz@gmail.com>
2022-10-06 12:33:04 -0700

    cleanup

--- config/passport.js
+++ config/passport.js
@@ -1,43 +1,45 @@
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-const User = require('../models/user');
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
+const User = require("../models/user");
 
 // configuring Passport!
-passport.use(new GoogleStrategy({
-  clientID: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_SECRET,
-  callbackURL: process.env.GOOGLE_CALLBACK
-},
-  async function (accessToken, refreshToken, profile, cb) { // verify callback that gets invoked every single time someone logs in
-    console.log(profile, " <- this is the profile from google")
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK,
+    },
+    async function ( profile, cb) {
+      // verify callback that gets invoked every single time someone logs in
+      console.log(profile, " <- this is the profile from google");
 
-    // Step 1, Check if the user exist in the database!
-    // if the do, provide that user document to the passport!
-    const user = await User.findOne({ googleId: profile.id });
-    // if User.findOne finds nothing, user will be undefined
+      // Step 1, Check if the user exist in the database!
+      // if the do, provide that user document to the passport!
+      const user = await User.findOne({ googleId: profile.id });
+      // if User.findOne finds nothing, user will be undefined
 
-    // cb(error, dataThatYouWantToPassToPassport)
-    if (user) return cb(null, user);
-
-    // So the User doens't exist in the database, 
-    // which means we have a new user, so we have to add them to the database!
-    try {
-      const newUser = await User.create({
-        name: profile.displayName,
-        googleId: profile.id,
-        email: profile.emails[0].value, // <- this give us the email
-        avatar: profile.photos[0].value //< - the hosted image string/link
-      })
-      // pass the newUser document to passport!
-      return cb(null, newUser)
-
-    } catch (err) {
       // cb(error, dataThatYouWantToPassToPassport)
-      return cb(err)
+      if (user) return cb(null, user);
+
+      // So the User doens't exist in the database,
+      // which means we have a new user, so we have to add them to the database!
+      try {
+        const newUser = await User.create({
+          name: profile.displayName,
+          googleId: profile.id,
+          email: profile.emails[0].value, // <- this give us the email
+          avatar: profile.photos[0].value, //< - the hosted image string/link
+        });
+        // pass the newUser document to passport!
+        return cb(null, newUser);
+      } catch (err) {
+        // cb(error, dataThatYouWantToPassToPassport)
+        return cb(err);
+      }
     }
-  }
-)
-)
+  )
+);
 
 passport.serializeUser(function (user, done) {
   done(null, user.id);
@@ -46,10 +48,7 @@ passport.serializeUser(function (user, done) {
 // This is where the user document is assigned to req.user
 passport.deserializeUser(function (id, done) {
   User.findById(id, function (err, userDocument) {
-    if (err) return done(err)
-    done(null, userDocument);  // <- this assins the userDocument to req.user = userDocument
-  })
+    if (err) return done(err);
+    done(null, userDocument); // <- this assins the userDocument to req.user = userDocument
+  });
 });
-
-
-
--- controllers/teams.js
+++ controllers/teams.js
@@ -2,133 +2,134 @@ const League = require("../models/league");
 const Player = require("../models/player");
 
 module.exports = {
-    create,
-    delete: deleteTeam,
-    show,
-    edit,
-    update,
+  create,
+  delete: deleteTeam,
+  show,
+  edit,
+  update,
 };
 
 // Update Team
 function update(req, res) {
-
-    Player.find({}, function (err, allPlayers) {
-        League.findOneAndUpdate({ "teams._id": req.params.id }, req.body,
-            function (err, teamDoc) {
-                // Cycle through all the teams in the league
-                for (let i = 0; i < teamDoc.teams.length; i++) {
-                    // Converts the ID path from an object to a string
-                    teamPath = JSON.stringify(teamDoc.teams[i]._id);
-                    // If the path matches the request by the client
-                    if (teamPath === `"${req.params.id}"`) {
-                        teamDoc.teams[i].teamName = req.body.teamName;
-                        teamDoc.teams[i].contactName = req.body.contactName;
-                        teamDoc.teams[i].contactEmail = req.body.contactEmail;
-                        teamDoc.teams[i].captainName = req.body.captainName;
-                        teamDoc.teams[i].captainEmail = req.body.captainEmail;
-                        teamDoc.save(function (err) {
-                            res.render('teams/show.ejs', {
-                                team: teamDoc.teams[i],
-                                league: teamDoc,
-                                allPlayers,
-                                players: teamDoc.teams[i].players
-                            });
-                        });
-                    };
-                };
+  Player.find({}, function (err, allPlayers) {
+    League.findOneAndUpdate(
+      { "teams._id": req.params.id },
+      req.body,
+      function (err, teamDoc) {
+        // Cycle through all the teams in the league
+        for (let i = 0; i < teamDoc.teams.length; i++) {
+          // Converts the ID path from an object to a string
+          teamPath = JSON.stringify(teamDoc.teams[i]._id);
+          // If the path matches the request by the client
+          if (teamPath === `"${req.params.id}"`) {
+            teamDoc.teams[i].teamName = req.body.teamName;
+            teamDoc.teams[i].contactName = req.body.contactName;
+            teamDoc.teams[i].contactEmail = req.body.contactEmail;
+            teamDoc.teams[i].captainName = req.body.captainName;
+            teamDoc.teams[i].captainEmail = req.body.captainEmail;
+            teamDoc.save(function (err) {
+              res.render("teams/show.ejs", {
+                team: teamDoc.teams[i],
+                league: teamDoc,
+                allPlayers,
+                players: teamDoc.teams[i].players,
+              });
             });
-    })
-};
+          }
+        }
+      }
+    );
+  });
+}
 
 // Edit team
 function edit(req, res) {
-    League.findOne({ 'teams._id': req.params.id },
-        function (err, teamDoc) {
-            // Cycle through all the teams in the league
-            for (let i = 0; i < teamDoc.teams.length; i++) {
-                // Converts the ID path from an object to a string
-                teamPath = JSON.stringify(teamDoc.teams[i]._id);
-                // If the path matches the request by the client
-                if (teamPath === `"${req.params.id}"`) {
-                    res.render('teams/edit.ejs', {
-                        team: teamDoc.teams[i],
-                    });
-                };
-            };
+  League.findOne({ "teams._id": req.params.id }, function (err, teamDoc) {
+    // Cycle through all the teams in the league
+    for (let i = 0; i < teamDoc.teams.length; i++) {
+      // Converts the ID path from an object to a string
+      teamPath = JSON.stringify(teamDoc.teams[i]._id);
+      // If the path matches the request by the client
+      if (teamPath === `"${req.params.id}"`) {
+        res.render("teams/edit.ejs", {
+          team: teamDoc.teams[i],
         });
+      }
+    }
+  });
 }
-
 
 // Show team
 function show(req, res) {
-    let teamPath = '';
-    let leaguePlayers = [];
+  let teamPath = "";
+  let leaguePlayers = [];
 
-    // Go through each team and add all the currrent players in the league
-    League.findOne({ 'teams._id': req.params.id, }, function (err, leagueDoc) {
-        for (let i = 0; i < leagueDoc.teams.length; i++) {
-            for (let n = 0; n < leagueDoc.teams[i].players.length; n++) {
-                leaguePlayers.push(leagueDoc.teams[i].players[n]);
-            }
+  // Go through each team and add all the currrent players in the league
+  League.findOne({ "teams._id": req.params.id }, function (err, leagueDoc) {
+    for (let i = 0; i < leagueDoc.teams.length; i++) {
+      for (let n = 0; n < leagueDoc.teams[i].players.length; n++) {
+        leaguePlayers.push(leagueDoc.teams[i].players[n]);
+      }
+    }
+  });
+
+  // Find the correct league that has the team selected
+  League.findOne({ "teams._id": req.params.id }, async function (err, teamDoc) {
+    // Cycle through all the teams in the league
+    for (let i = 0; i < teamDoc.teams.length; i++) {
+      // Converts the ID path from an object to a string
+      teamPath = JSON.stringify(teamDoc.teams[i]._id);
+      // If the path matches the request by the client
+      if (teamPath === `"${req.params.id}"`) {
+        let players = [];
+
+        for (let n = 0; n < teamDoc.teams[i].players.length; n++) {
+          let player = await Player.findOne({
+            _id: teamDoc.teams[i].players[n],
+          });
+          players.push(player);
         }
-    })
-
-    // Find the correct league that has the team selected
-    League.findOne({ 'teams._id': req.params.id, },
-        async function (err, teamDoc) {
-            // Cycle through all the teams in the league
-            for (let i = 0; i < teamDoc.teams.length; i++) {
-                // Converts the ID path from an object to a string
-                teamPath = JSON.stringify(teamDoc.teams[i]._id);
-                // If the path matches the request by the client
-                if (teamPath === `"${req.params.id}"`) {
-                    let players = [];
-
-                    for (let n = 0; n < teamDoc.teams[i].players.length; n++) {
-                        let player = await Player.findOne({ _id: teamDoc.teams[i].players[n] })
-                        players.push(player)
-                    }
-                    Player.find({ _id: { $nin: leaguePlayers } }, function (err, allPlayers) {
-
-                        res.render('teams/show.ejs', {
-                            team: teamDoc.teams[i],
-                            league: teamDoc,
-                            players,
-                            allPlayers
-                        });
-                    });
-                }
-            };
-        });
-};
+        Player.find(
+          { _id: { $nin: leaguePlayers } },
+          function (err, allPlayers) {
+            res.render("teams/show.ejs", {
+              team: teamDoc.teams[i],
+              league: teamDoc,
+              players,
+              allPlayers,
+            });
+          }
+        );
+      }
+    }
+  });
+}
 
 // Create team
 function create(req, res) {
-    League.findById(req.params.id, function (err, leagueDoc) {
+  League.findById(req.params.id, function (err, leagueDoc) {
+    //USE THIS FOR PROFILE STUFF
+    req.body.user = req.user._id;
+    req.body.userName = req.user.name;
+    req.body.userAvatar = req.user.avatar;
 
-        //USE THIS FOR PROFILE STUFF
-        req.body.user = req.user._id;
-        req.body.userName = req.user.name;
-        req.body.userAvatar = req.user.avatar;
+    leagueDoc.teams.push(req.body);
 
-        leagueDoc.teams.push(req.body);
-
-        leagueDoc.save(function (err) {
-            res.redirect(`/leagues/${req.params.id}`);
-        });
+    leagueDoc.save(function (err) {
+      res.redirect(`/leagues/${req.params.id}`);
     });
-};
+  });
+}
 
 //Delete team
 function deleteTeam(req, res) {
-    // Match team
-    League.findOne({ 'teams._id': req.params.id, },
-        function (err, leagueDoc) {
-            // no logged in user
-            if (!leagueDoc) return res.redirect('/leagues');
+  // Match team
+  League.findOne({ "teams._id": req.params.id }, function (err, leagueDoc) {
+    // no logged in user
+    if (!leagueDoc) return res.redirect("/leagues");
 
-            leagueDoc.teams.remove(req.params.id);
-            leagueDoc.save();
-            res.redirect(`/leagues/${leagueDoc._id}`)
-        });
+    leagueDoc.teams.remove(req.params.id);
+    leagueDoc.save();
+    res.redirect(`/leagues/${leagueDoc._id}`);
+  });
 }
